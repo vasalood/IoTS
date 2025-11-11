5053187d46033c8fac8704a8d4cc947023686113
Vasilije Tomovic <vasilije.tomovic@elfak.rs>
2025-11-11 15:11:13 +0100

    Removed

--- Projekat 3/ArduinoSketch/sketch_iots_project3/secrets.h
+++
@@ -1,6 +0,0 @@
-const char WIFI_SSID[] = "wifi";
-const char WIFI_PASS[] = "pitajmene";
-const char MQTT_BROKER[] = "172.16.1.12";  // ili "192.168.0.10" ako koristiš svoj Mosquitto
-const int  MQTT_PORT = 1883;
-const char MQTT_TOPIC[] = "sensor/data";
-const char SENSOR_NAME[] = "MKR1010_WiFi";
